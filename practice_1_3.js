ad48056363bcec3da8614baf6ae4b0540a804d75
Eugenius Tsoy <evgeniustsoy1308@gmail.com>
2022-01-27 22:17:56 +0300

    added first practical work

---
+++ practice_1_3.js
@@ -0,0 +1,23 @@
+let str = "Hello";
+let num = 5;
+let bool = true;
+
+//Сложение 
+console.log(str + num);
+console.log(str + num);
+console.log(num + bool);
+
+//Умножение
+console.log(str * num);
+console.log(str * num);
+console.log(num * bool);
+
+//Деление
+console.log(str / num);
+console.log(str / num);
+console.log(num / bool);
+
+//Преобразование
+console.log(Number (str));
+console.log(Boolean (str));
+console.log(String (str));
